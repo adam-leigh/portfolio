92732289cfb11366e4b115c8d10f3fa98f155908
Adam Leigh <adam@fluently.tech>
2024-10-17 08:03:08 +0200

    Landing page: adding <br/> to title

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ export default function Page() {
 	return (
 		<section>
 			<h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-				Speak the language of technology, fluently.
+				Speak the language of technology,
+				<br />
+				fluently.
 			</h1>
 			<p className="mb-4">
 				{`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
